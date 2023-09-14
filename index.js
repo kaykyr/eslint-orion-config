6503497bfe88ccfd2f7f2dd1b0058aaf6f19f7e8
Kayky Ramos <kaykyramos@Kaykys-iMac-Pro.local>
2023-09-14 17:42:35 -0300

    Updates

--- index.js
+++ index.js
@@ -1,74 +1,78 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true,
-        jest: true,
-        browser: true,
-        jest: true,
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier-plugin-tailwindcss/recommended",
+    "standard",
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-        'plugin:react/recommended',
-        'plugin:react-hooks/recommended',
-        'plugin:prettier-plugin-tailwindcss/recommended',
-        'standard',
-        'plugin:prettier/recommended',
-        "prettier"
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: [
+    "react",
+    "jsx-a11y",
+    "@typescript-eslint",
+    "prettier",
+    "import-helpers",
+  ],
+  rules: {
+    "prettier/prettier": "error",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+    "jsx-a11y/alt-text": [
+      "warn",
+      {
+        elements: ["img"],
+        img: ["Image"],
+      },
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true
-        },
-        ecmaVersion: 12,
-        sourceType: "module"
-    },
-    plugins: [
-        'react',
-        'jsx-a11y',
-        '@typescript-eslint',
-        'prettier',
-        'import-helpers'
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        newlinesBetween: "always",
+        groups: [
+          ["module", "absolute"],
+          "/^@/",
+          ["parent", "sibling", "index"],
+        ],
+        alphabetize: { order: "asc", ignoreCase: true },
+      },
     ],
-    rules: {
-        'prettier/prettier': 'error',
-        'react/react-in-jsx-scope': 'off',
-        'react/prop-types': 'off',
-        'jsx-a11y/alt-text': [
-            'warn',
-            {
-                elements: ['img'],
-                img: ['Image'],
-            },
-        ],
-        "import-helpers/order-imports": [
-            "warn",
-            {
-                "newlinesBetween": "always",
-                "groups": [
-                    ["module", "absolute"],
-                    "/^@/",
-                    ["parent", "sibling", "index"]
-                ],
-                "alphabetize": { "order": "asc", "ignoreCase": true }
-            }
-        ],
-        "no-useless-constructor": "off",
-        "no-new": "off",
-        "camelcase": "off",
-        "no-unused-vars": "off",
-        "semicolon": "error",
-        'jsx-a11y/aria-props': 'warn',
-        'jsx-a11y/aria-proptypes': 'warn',
-        'jsx-a11y/aria-unsupported-elements': 'warn',
-        'jsx-a11y/role-has-required-aria-props': 'warn',
-        'jsx-a11y/role-supports-aria-props': 'warn',
+    "no-useless-constructor": "off",
+    "no-new": "off",
+    camelcase: "off",
+    "no-void": "off",
+    "no-prototype-builtins": "off",
+    "no-unused-vars": "off",
+    "no-case-declarations": "off",
+    "array-callback-return": "off",
+    semicolon: "error",
+    "jsx-a11y/aria-props": "warn",
+    "jsx-a11y/aria-proptypes": "warn",
+    "jsx-a11y/aria-unsupported-elements": "warn",
+    "jsx-a11y/role-has-required-aria-props": "warn",
+    "jsx-a11y/role-supports-aria-props": "warn",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    settings: {
-        react: {
-            version: 'detect',
-        },
-        'import/parsers': {
-            [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
-        },
+    "import/parsers": {
+      [require.resolve("@typescript-eslint/parser")]: [".ts", ".tsx", ".d.ts"],
     },
-}
+  },
+};
